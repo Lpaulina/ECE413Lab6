0d9756de4cd24c21c938cc8fe6ca6db686b81be5
Paulina Lujan <paulina2801@icloud.com>
2024-11-20 00:07:11 -0700

    Posts & gets for lab work properly

--- routes/lab.js
+++ routes/lab.js
@@ -4,50 +4,59 @@ var Recording = require("../models/lab");
 
 router.get('/status', async function(req,res){
     var zipCode = req.query.zip;
-    if (!zipCode){
-        res.status(400).json({"error": "a zip code is required"});
-        return;
+    if (!zipCode.length){
+        var errormsg = {error : "a zip code is required."}
+        res.status(400).json(errormsg);
     }
-    Recording.find(function(err, recordings){
+    else{
+    Recording.find({ zip: req.query.zip}, function(err, recordings){
         if (err){
-            res.status(400).json({"error" : err});
+            res.status(400).json({error : "a zip code is required."});
         }
         else{
-            if (recordings.length == 0){
-                res.status(400).json({"error" : "Zip does not exist in the database."});
-                return;
+            if (recordings.length === 0){
+                var errormsg = {error : "Zip does not exist in the database."}
+                res.status(400).json(errormsg);
             }
-            let avg = 0;
-            for(const recording of recordings){
-                avg += recording.airQuality;
-            }
-            avg /= recordings.length;
-        
-            res.status(200).json({avg});
+            else {
+                let avg = 0;
+                for(const recording of recordings){
+                    avg += recording.airQuality;
+                }
+                avg /= recordings.length;
+                avg = avg.toFixed(2);
+            
+                res.status(200).json(avg);
         }
+        }
+        
     })
-    
+}
 
 })
 
+
 router.post('/register',  function(req,res){
     if (!req.body.zip || ! req.body.airQuality){
-        res.status(400).json({"error": "zip and airQuality are required."})
+        var errormsg = {error : "zip and airQuality are required."}
+        res.status(400).json(errormsg)
 
-        return;
     }
-    const newRecording = new Recording({
-        zip : req.body.zip,
-        airQuality : req.body.airQuality
-    })
-    newRecording.save(function (err, student){
-        if (err){
-            res.status(400).send(err);
-        }
-        else{
-            res.status(201).json({"response": "Data recorded."})
-        }
-    });
+    else{
+        const newRecording = new Recording({
+            zip : req.body.zip,
+            airQuality : req.body.airQuality
+        })
+        newRecording.save(function (err, student){
+            if (err){
+                res.status(400).json({error : "zip and airQuality are required."});
+            }
+            else{
+                var msg = {response : "Data recorded."}
+                res.status(201).json(msg)
+            }
+        });
+    }
 
 })
 
